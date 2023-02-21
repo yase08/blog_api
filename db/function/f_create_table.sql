0de006301a9c02e44c899e15da64b57bcd66827f
Marjan02 <kevinzevalja@gmail.com>
2023-02-22 03:03:48 +0700

    nal

--- db/function/f_create_table.sql
+++ db/function/f_create_table.sql
@@ -51,7 +51,6 @@ BEGIN
         email VARCHAR(255) NOT NULL UNIQUE,
         created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        deleted_at TIMESTAMP,
         avatar VARCHAR(255)
     );
 END;
